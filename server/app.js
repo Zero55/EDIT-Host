d4e08740044ebc0dd326603c9432315670afdce1
Jan Kocbek <Jan.Kocbek@datalex.com>
2019-07-04 17:19:25 +0200

    implementation with testing client on gui

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const WebSocket = require("ws");
 
-// SERVER
+// server
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
@@ -12,17 +12,53 @@ app.use(express.static("./dist/"));
 
 app.listen(3000, () => console.log("Server running on port 3000!"));
 
-// WEBSOCKET
-const ws = new WebSocket.Server({ port: 8080 });
+// websocket - host gui
+const wsHostGui = new WebSocket.Server({ port: 8081 });
 
-ws.on("connection", function connection(connection) {
-  connection.on("message", function incoming(message) {
-    console.log("Message recieved: %s", message);
-  });
+// websocket - clients
+const wsClients = new WebSocket.Server({ port: 8080 });
 
-  connection.send(JSON.stringify("something"));
+wsClients.on("connection", function connection(connection) {
+  let egmName = "";
 
-  connection.on("close", function close() {
-    console.log("Connection closed.");
+  connection.on("message", function incoming(incoming) {
+    const message = JSON.parse(incoming);
+    let logMessage, responseCommand;
+
+    switch (message.command) {
+      case "commsOnLine":
+        egmName = message.name;
+        logMessage = `${egmName} - online.`;
+        responseCommand = "commsOnLineAck";
+        break;
+      case "heartbeat":
+        logMessage = `${egmName} - heartbeat (${message.sessionId}).`;
+        responseCommand = "heartbeatAck";
+        break;
+      case "reportMeter":
+        logMessage = `${egmName} - report meter (${message.name} = ${message.value}).`;
+        responseCommand = "meterAck";
+        break;
+      case "reportEvent":
+        logMessage = `${egmName} - report event (${message.name}).`;
+        responseCommand = "eventAck";
+        break;
+      case "reportStatus":
+        logMessage = `${egmName} - report status (${message.value}).`;
+        responseCommand = "statusAck";
+        break;
+      default:
+        return;
+    }
+
+    connection.send(JSON.stringify({ command: responseCommand }));
+
+    wsHostGui.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(logMessage));
+      }
+    });
+
+    console.log(logMessage);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { WebsocketService } from "./services/websocket.service";
-import { Observable, Subscription } from "rxjs";
+import { Subscription } from "rxjs";
+import { WebSocketSubject, webSocket } from "rxjs/webSocket";
 
 @Component({
   selector: "app-root",
@@ -8,24 +8,82 @@ import { Observable, Subscription } from "rxjs";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = "EDIT 2019";
-  messages = [];
-  messages$: Observable<any>;
-  subscription: Subscription;
+  public title = "EDIT 2019";
+  public messages = [];
 
-  constructor(private websocketService: WebsocketService) {}
+  private webSocketUrl = "ws://blueberry.comtrade.com:8081/";
+  private webSocketSubject: WebSocketSubject<any>;
+  private subscription: Subscription;
+
+  private testClientWebSocketUrl = "ws://blueberry.comtrade.com:8080/";
+  private testClientwebSocketSubject: WebSocketSubject<any>;
+  private testClientSubscription: Subscription;
+
+  constructor() {}
 
   ngOnInit() {
-    this.messages$ = this.websocketService.create(
-      "ws://blueberry.comtrade.com:8080/"
+    this.testClientwebSocketSubject = webSocket(this.testClientWebSocketUrl);
+    this.testClientSubscription = this.testClientwebSocketSubject
+      .asObservable()
+      .subscribe(
+        message => console.log(`WebSocket message: ${JSON.stringify(message)}`),
+        error => console.error(`WebSocket error: ${JSON.stringify(error)}`),
+        () => console.log("WebSocket connection closed.")
+      );
+
+    this.webSocketSubject = webSocket(this.webSocketUrl);
+    this.subscription = this.webSocketSubject.asObservable().subscribe(
+      message => {
+        this.messages.push(message);
+      },
+      error => console.error(`WebSocket error: ${JSON.stringify(error)}`),
+      () => console.log("WebSocket connection closed.")
     );
-    this.subscription = this.messages$.subscribe(message => {
-      this.messages.push(message);
-      this.websocketService.send("HELOOOOOOOOO");
-    });
   }
 
   ngOnDestroy() {
+    this.testClientSubscription.unsubscribe();
+
     this.subscription.unsubscribe();
+  }
+
+  send(command) {
+    let message;
+    switch (command) {
+      case "commsOnLine":
+        message = {
+          command: "commsOnLine",
+          name: "EGM_" + Date.now()
+        };
+        break;
+      case "heartbeat":
+        message = {
+          command: "heartbeat",
+          sessionId: "sessiodId"
+        };
+        break;
+      case "reportMeter":
+        message = {
+          command: "reportMeter",
+          name: "gameWon",
+          value: "10"
+        };
+        break;
+      case "reportEvent":
+        message = {
+          command: "reportEvent",
+          name: "logicDoorOpen"
+        };
+        break;
+      case "reportStatus":
+        message = {
+          command: "reportStatus",
+          value: "enable"
+        };
+        break;
+      default:
+        return;
+    }
+    this.testClientwebSocketSubject.next(message);
   }
 }
